e46b8d0d3e68b9b22a633e27e6344576c6de8dd8
seo <syh05247@naver.com>
2022-09-24 01:42:11 +0900

    Basic login register

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,61 @@
+package com.example.demo.controller;
+
+import com.example.demo.dto.ResponseDTO;
+import com.example.demo.dto.UserDTO;
+import com.example.demo.model.UserEntity;
+import com.example.demo.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.catalina.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@Slf4j
+@RestController
+@RequestMapping("/auth")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> registerUser(@RequestBody UserDTO userDTO){
+        try{
+            UserEntity user = UserEntity.builder()
+                    .email(userDTO.getEmail())
+                    .username(userDTO.getUsername())
+                    .password(userDTO.getPassword())
+                    .build();
+
+            UserEntity registerUser = userService.create(user);
+            UserDTO responseUserDTO = UserDTO.builder()
+                    .email(registerUser.getEmail())
+                    .id(registerUser.getId())
+                    .username(registerUser.getUsername())
+                    .build();
+            return ResponseEntity.ok().body(responseUserDTO);
+        }catch (Exception e) {
+            ResponseDTO responseDTO = ResponseDTO.builder().error(e.getMessage()).build();
+            return ResponseEntity.badRequest().body(responseDTO);
+        }
+    }
+
+    @PostMapping("/signin")
+    public ResponseEntity<?> authenticate(@RequestBody UserDTO userDTO){
+        UserEntity user = userService.getByCredentials(userDTO.getEmail(), userDTO.getPassword());
+
+        if(user != null){
+            final UserDTO responseUserDTO = UserDTO.builder()
+                    .email(user.getEmail())
+                    .id(user.getId())
+                    .build();
+            return ResponseEntity.ok().body(responseUserDTO);
+        }else {
+            ResponseDTO responseDTO = ResponseDTO.builder()
+                    .error("Login failed")
+                    .build();
+            return ResponseEntity.badRequest().body(responseDTO);
+        }
+    }
+}
